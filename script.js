90c3d5fd161d892c42ce4c2da847b557fdc3115c
Vitaliy0973 <arkodianec1@gmail.com>
2023-08-09 17:43:56 +0300

    task completed

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+{
+  const product = 'Aser Aspire 3';
+  const quantityProduct = 10;
+  const category = 'laptop';
+  const price = 35000;
+
+
+  //  Вывод в консоль названия продукта и общей стоимости товаров
+  console.log('product: ', product);
+  console.log(`Общая сумма товаров: ${quantityProduct * price}`);
+}
+
+{
+  const product = 'Hyundai CC3595FIX';
+  const quantityProduct = 13;
+  const category = 'fridge';
+  const price = 40570;
+
+
+  //  Вывод в консоль названия продукта и общей стоимости товаров
+  console.log('product: ', product);
+  console.log(`Общая сумма товаров: ${quantityProduct * price}`);
+}
