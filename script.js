c7e0e66731108aeadba4a0623449d0986cf52920
Vitaliy0973 <arkodianec1@gmail.com>
2023-08-09 17:28:17 +0300

    task completed

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const product = 'Aser Aspire 3';
+const quantityProduct = 10;
+const category = 'laptop';
+const price = 35000;
+
+
+//  Вывод в консоль названия продукта и общей стоимости товаров
+console.log('product: ', product);
+console.log(`Общая сумма товаров: ${quantityProduct * price}`);
+
