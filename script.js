c4a8c301010e421bc7a42db3b99e32bcb008ca74
Vitaliy0973 <arkodianec1@gmail.com>
2023-08-10 21:19:39 +0300

    task completed

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 
 
   //  Вывод в консоль названия продукта и общей стоимости товаров
-  console.log('product: ', product);
+  console.log('Наименование товара:  ', product);
   console.log(`Общая сумма товаров: ${quantityProduct * price}`);
 }
 
@@ -20,6 +20,33 @@
 
 
   //  Вывод в консоль названия продукта и общей стоимости товаров
-  console.log('product: ', product);
+  console.log('Наименование товара: ', product);
   console.log(`Общая сумма товаров: ${quantityProduct * price}`);
+}
+
+{
+  const product = prompt('Наименование товара: ');
+  const quantityProduct = Number(prompt('Количество товара: '));
+  const category = prompt('Категория товара: ');
+  const price = Number(prompt('Цена товара в рублях: '));
+
+
+  // Вывод в консоль ранее полученной от пользователя информации
+  console.log(`Наименование товара: ${product}`);
+
+  if (!Number.isFinite(quantityProduct)) {
+    console.log(`Количество товара: Было введено не число`);
+  } else {
+    console.log(`Количество товара: ${quantityProduct}`);
+  }
+
+  console.log(`Категория товара: ${category}`);
+
+  if (!Number.isFinite(price)) {
+    console.log(`Цена товара в рублях: Было введено не число`);
+  } else {
+    console.log(`Цена товара в рублях: ${price}`);
+  }
+
+  console.log(`На складе ${quantityProduct} единицы товара "${product}" на сумму ${quantityProduct * price} деревянных`);
 }
