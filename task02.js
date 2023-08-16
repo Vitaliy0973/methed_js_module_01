f73a7d507806eaa4814616084657e60be6919e96
Vitaliy0973 <arkodianec1@gmail.com>
2023-08-16 14:07:14 +0300

    fix completed

--- task02.js
+++ task02.js
@@ -1,13 +1,13 @@
 'use strict';
 
 {
+    const allСashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
+
     const getAverageValue = (arr) => {
         const totalSum = arr.reduce((sum, elem) => sum + elem);
 
         return Math.floor(totalSum / arr.length);
     }
-
-    const allСashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
 
     console.log(getAverageValue(allСashbox));
 }
--- task03.js
+++ task03.js
@@ -1,11 +1,11 @@
 'use strict';
 
 {
+    const names = ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
+
     const addPrefix = (arr, str) => {
         return arr.map((elem) => `${str} ${elem}`);
     }
-
-    const names = ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
 
     console.log(addPrefix(names, 'Mr'));
 }
