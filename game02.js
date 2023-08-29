f94ee18fc5316d2122768036f3be241713d6a90a
Vitaliy0973 <arkodianec1@gmail.com>
2023-08-29 18:26:00 +0300

    fix completed

--- game02.js
+++ game02.js
@@ -1,96 +1,86 @@
 'use strict';
 
 {
-    const getUserData = (text) => {
-        let message = text;
+  const getUserData = (text) => {
+    const message = 'Введи число!';
+    const data = prompt(`${text}`);
 
-        while (true) {
-            let data = prompt(`${message}`);
-
-            if (data === null) {
-                return null;
-            } else if (isNaN(data) || data === '') {
-                message = 'Введи число!';
-            } else if (Number.isFinite(+data)) {
-                return +data;
-            }
-        }
+    if (data === null) {
+      return null;
+    } else if (!Number.isNaN(parseFloat(data)) && isFinite(data)) {
+      return +data;
     }
 
+    return getUserData(message);
+  };
 
-    const getUserRound = () => {
-        const arr = []
-        const messages = ['Введите первое число', 'Введите второе число'];
-        for (let i = 0; i < 2; i++) {
-            let userNumber = getUserData(messages[i]);
-            if (userNumber === null) {
-                return [null, null];
-            }
-            arr.push(userNumber);
-        }
-        return arr;
+  const getUserRound = () => {
+    const messages = ['Введите первое число', 'Введите второе число'];
+
+    return messages.reduce((intervel, message) => {
+      intervel.push(intervel.includes(null) ? null : getUserData(message));
+
+      return intervel;
+    }, []);
+  };
+
+  const getRandomNumber = (n, m) => {
+    const min = Math.min(n, m);
+    const max = Math.max(n, m);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  const getCount = (n, m) => {
+    const min = Math.min(n, m);
+    const max = Math.max(n, m);
+
+    return Math.round((max - min) * 0.3);
+  };
+
+  const play = (message, arrUserNumbers, num, count) => {
+    if (arrUserNumbers.length === count) {
+      return alert('У вас закончились попытки. Вы проиграли!!');
     }
 
+    const userNumber = getUserData(message);
 
-    const getRandomNumber = (n, m) => {
-        const min = Math.min(n, m);
-        const max = Math.max(n, m);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+    if (userNumber === null) {
+      return alert('Игра окончена!');
+    } else if (arrUserNumbers.includes(userNumber)) {
+      message = 'Это число вы уже вводили! Введите другое число';
+    } else {
+      switch (true) {
+        case userNumber > num:
+          message = 'Меньше! Введите число';
+          break;
+        case userNumber < num:
+          message = 'Больше! Введите число';
+          break;
+        default:
+          return alert('Правильно!!');
+      }
+
+      arrUserNumbers.push(userNumber);
     }
 
+    return play(message, arrUserNumbers, num, count);
+  };
 
-    const getCount = (n, m) => {
-        const min = Math.min(n, m);
-        const max = Math.max(n, m);
-        return Math.round((max - min) * 0.3);
+
+  const guessNumber = () => {
+    const message = 'Введите число';
+    const arrUserNumbers = [];
+    const [userNumberOne, userNumberTwo] = getUserRound();
+    const num = getRandomNumber(userNumberOne, userNumberTwo);
+    const count = getCount(userNumberOne, userNumberTwo);
+
+    if (userNumberOne === null || userNumberTwo === null) {
+      return alert('Игра окончена!');
     }
 
+    play(message, arrUserNumbers, num, count);
+  };
 
-    const guessNumber = () => {
-        const arrUserNumbers = []
-        let message = 'Введите число';
-        // По моей логике я должен остановить программу если пользователь нажал отмена.
-        // Запрашивать второе число в таком случае будет не правильно.
-        // Вариант объявить пустые переменные в начале кода, а потом присвоить им
-        // значения мне кажется ты забракуешь. Вот через функцию. Выглядит сложнее)
-        const [userNumberOne, userNumberTwo] = getUserRound();
-
-        if (userNumberOne === null || userNumberTwo === null) {
-            return alert('Игра окончена!');
-        }
-
-        const num = getRandomNumber(userNumberOne, userNumberTwo);
-        let count = getCount(userNumberOne, userNumberTwo);
-
-        while (arrUserNumbers.length !== count) {
-            let userNumber = getUserData(message);
-
-            if (arrUserNumbers.includes(userNumber)) {
-                message = 'Это число вы уже вводили! Введите другое число';
-            } else {
-                arrUserNumbers.push(userNumber);
-
-                switch (true) {
-                    // Не нравится мне идея запихивать это условие в эту часть кода
-                    // потому, что в таком случае этот null попадет в массив arrUserNumbers.
-                    case userNumber === null:
-                        return alert('Игра окончена!');
-                    case userNumber > num:
-                        message = 'Меньше! Введите число';
-                        break;
-                    case userNumber < num:
-                        message = 'Больше! Введите число';
-                        break
-                    // Мне кажется, что отсутствие явного «case userNumber === num»
-                    // ухудшает читаемость кода в данном случае.
-                    default:
-                        return alert('Правильно!!');
-                }
-            }
-        }
-
-        alert('У вас закончились попытки. Вы проиграли!!');
-    }
-
-    guessNumber();
+  guessNumber();
 }
